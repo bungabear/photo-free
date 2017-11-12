a0049f24eaca0500570ca22b6e65b8a8d2e8aea3
LeeYongseok <dyd9422@gmail.com>
2017-11-12 22:28:41 +0900

    upload image

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var busboy = require('connect-busboy');
+var path = require('path');
+var fs = require('fs-extra');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var upload = require('./routes/upload');
 
 var app = express();
 
@@ -23,7 +26,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/upload', upload);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
